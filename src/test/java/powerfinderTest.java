ce2bc119f4674713738268bf6e24d202b767938d
0xgoto <65106361+0xgoto@users.noreply.github.com>
2022-03-10 17:07:36 +0530

    improved naming conventions

--- src/test/java/powerfinderTest.java
+++ src/test/java/powerfinderTest.java
@@ -1,26 +1,25 @@
 import org.junit.*;
 
-import static org.assertj.core.api.Assertions.*;
 import powerpackage.powerfinder;
 
 public class powerfinderTest {
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void one_power_one_is_one() {
         assert powerfinder.calculation(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void two_power_is_two() {
         assert powerfinder.calculation(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void two_power_two_is_four() {
         assert powerfinder.calculation(2, 2) == 4;
     }
 
     @Test
-    public void power_calculation_2_and_3_Is_SIX() {
-        assert powerfinder.calculation(3, 2) == 3*3;
+    public void three_power_two_is_six() {
+        assert powerfinder.calculation(3, 2) == 9;
     }
 }
