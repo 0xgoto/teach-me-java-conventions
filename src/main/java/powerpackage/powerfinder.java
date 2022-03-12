e2bf223540b53e91897f024fc276e63986e72188
0xgoto <65106361+0xgoto@users.noreply.github.com>
2022-03-12 12:16:51 +0530

    improved the naming conventions

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,6 +1,6 @@
 package powerpackage;
 
-public class powerfinder {
+public class PowerFinder {
     public static int calculation(int base, int power) {
         int product_count = 1;
         for (int i = 0; i<power; i++) {
--- src/test/java/powerfinderTest.java
+++ src/test/java/powerfinderTest.java
@@ -1,25 +1,25 @@
 import org.junit.*;
 
-import powerpackage.powerfinder;
+import powerpackage.PowerFinder;
 
-public class powerfinderTest {
+public class PowerFinderTest {
     @Test
     public void one_power_one_is_one() {
-        assert powerfinder.calculation(1,1) == 1;
+        assert PowerFinder.calculation(1,1) == 1;
     }
 
     @Test
-    public void two_power_is_two() {
-        assert powerfinder.calculation(2,1) == 2;
+    public void two_power_one_is_two() {
+        assert PowerFinder.calculation(2,1) == 2;
     }
 
     @Test
     public void two_power_two_is_four() {
-        assert powerfinder.calculation(2, 2) == 4;
+        assert PowerFinder.calculation(2, 2) == 4;
     }
 
     @Test
     public void three_power_two_is_six() {
-        assert powerfinder.calculation(3, 2) == 9;
+        assert PowerFinder.calculation(3, 2) == 9;
     }
 }
